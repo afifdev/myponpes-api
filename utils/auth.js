7b0196997c93cf1dfab6558cba8b58bde883de19
afifvdin <afifudin.fdn@gmail.com>
2021-05-12 21:40:20 +0700

    adding auth

---
+++ utils/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split("Bearer ")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.body._id = user._id;
+        return next();
+      } catch (err) {
+        const errors = new Error();
+        errors.message = "Invalid/Expired token";
+        return next(errors);
+      }
+    }
+    const errors = new Error();
+    errors.message = "Token should be: Bearer [token]";
+    return next(errors);
+  }
+  const errors = new Error();
+  errors.message = "Token must be provided";
+  return next(errors);
+};
+
+module.exports = auth;
