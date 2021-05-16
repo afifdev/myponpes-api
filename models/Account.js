ae6691b9aa85f65b25f25151c4c8113595c04809
afifvdin <afifudin.fdn@gmail.com>
2021-05-17 00:09:43 +0700

    remove transaction to account because i was forgot

--- models/Account.js
+++ models/Account.js
@@ -4,10 +4,19 @@ const Account = new Schema({
   balance: String,
   transaction: [
     {
-      type: String,
-      name: String,
-      value: String,
+      title: String,
+      desc: String,
+      amount: String,
+      due_date: String,
       ref_code: String,
+      users: [
+        {
+          user_id: String,
+          payment_date: String,
+          image: String,
+          is_complete: Boolean,
+        },
+      ],
     },
   ],
   planner: [
