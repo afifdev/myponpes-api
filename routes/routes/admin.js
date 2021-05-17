53fff36f16a38392d7fa13a22c513aeb496d1daa
afifvdin <afifudin.fdn@gmail.com>
2021-05-17 10:08:17 +0700

    adding transaction route over admin and cleaning code and formHandle

--- routes/routes/admin.js
+++ routes/routes/admin.js
@@ -8,22 +8,24 @@ const upload = multer({
   storage: fileStorage,
   fileFilter: fileFilter,
 }).single("image");
+const formHandler = multer().none();
 
 // http://localhost:4000/api/admin
 
-// [POST]
-routes.post("/", upload, adminControllers.login);
+routes.post("/", formHandler, adminControllers.login);
 
-// [GET]
-routes.get("/user", upload, auth, adminControllers.getUsers);
+// ----------------------- USER
 
-// [POST]
+routes.get("/user", formHandler, auth, adminControllers.getUsers);
 routes.post("/user", upload, auth, adminControllers.registerUser);
-
-// [PUT]
 routes.put("/user/:id", upload, auth, adminControllers.updateUser);
+routes.delete("/user/:id", formHandler, auth, adminControllers.deleteUser);
 
-// [DELETE]
-routes.delete("/user/:id", upload, auth, adminControllers.deleteUser);
+// ------------------------ TRANSACTION
+
+routes.get("/transaction", auth, adminControllers.getTransactions);
+routes.get("/transaction/:id", auth, adminControllers.getTransaction);
+routes.post("/transaction", upload, auth, adminControllers.addTransaction);
+routes.delete("/transaction/:id", auth, adminControllers.deleteTransaction);
 
 module.exports = routes;
