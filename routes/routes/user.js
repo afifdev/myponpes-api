4f7be09ff1866236f4fa7bf8d8c6f532fe783475
afifvdin <afifudin.fdn@gmail.com>
2021-05-17 10:13:57 +0700

    using formHandler on non-uploaded image route

--- routes/routes/user.js
+++ routes/routes/user.js
@@ -15,11 +15,12 @@ const uploads = multer({
   fileFilter,
   storage: multifileStorage,
 }).array("images");
+const formHandler = multer().none();
 const userControllers = require("../controllers/userController");
 
 // http://localhost:4000/api/user
 
-routes.post("/login", upload, userControllers.loginUser);
+routes.post("/login", formHandler, userControllers.loginUser);
 routes.get("/", auth, userControllers.getUser);
 routes.put("/", upload, auth, userControllers.updateUser);
 
